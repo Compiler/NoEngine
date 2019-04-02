98bcd274f7eebb36eb6069b0cadcac7a785bb440
Luke <lr98lr@aol.com>
2019-04-02 09:19:48 -0400

    some bullshit 3d testing

--- NoEngineHere/NoEngineHere/main.cpp
+++ NoEngineHere/NoEngineHere/main.cpp
@@ -4,10 +4,19 @@
 #include <glm/vec3.hpp>
 #include "Shader.h"
 #include "Triangle.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+bool w_pressed;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode){
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	if(key == GLFW_KEY_W && action == GLFW_PRESS)
+		w_pressed = true;
+	if(key == GLFW_KEY_W && action == GLFW_RELEASE)
+		w_pressed = false;
 }
 
 
@@ -17,7 +26,6 @@ int main(){
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//setting context to ?.3      = 3.3
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
 	GLFWwindow *window = glfwCreateWindow(800, 600, "NoEngine ", nullptr, nullptr);//returns pointer to GLFWwindow object
 
 	//make sure window creation worked
@@ -38,16 +46,22 @@ int main(){
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
-
+	std::cout << width;
 	glfwSetKeyCallback(window, key_callback);
 
-	Shader our_shader("shader_v.glsl", "shader_f.glsl");
+	Shader ourShader("shader_v.glsl", "shader_f.glsl");
+	//Shader movement("shader_v.glsl", "shader_f.glsl");
+	
+	
 
 	GLfloat vertices[] = {
-		// positions			// colors
-		-1.0f, -1.0f, 0.0f,		1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f
+		// positions	
+		 -0.5f, -0.5f, 0.0f,	0.0f, -0.5f, 0.0f,	0.0f, 0.5f, 0.25f,
+		 -0.5f, -0.5f, 0.0f,	0.0f, 0.5f, 0.5f,	0.0f, 0.5f, 0.25f,
+		 0.0f, -0.5f, 0.0f,		0.5f, -0.5f, 0.0f,	0.0f, 0.5f, 0.25f,
+		 0.5f, -0.5f, 0.0f,		0.0f, 0.5f, 0.5f,	0.0f, 0.5f, 0.25f,
+		 -0.5f, -0.5f, 0.0f,	0.5f, -0.5f, 0.0f,	0.0f, 0.5f, 0.5f
+		 //0.75f, 0.75f, 0.0f,	1.0f, 0.75f, 0.0f,	0.875f, 1.0f, 0.0f
 	};
 
 
@@ -65,27 +79,52 @@ int main(){
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// set position attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// set color attribute pointers
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
 
 	// unbind the vertex array object
 	glBindVertexArray(0);
 
+
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 view = glm::mat4(1.0f);
+	// note that we're translating the scene in the reverse direction of where we want to move
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
+
+
+	
+
 	while (!glfwWindowShouldClose(window))
 	{
+		if(w_pressed)
+			std::cout << "w action" << std::endl;
 		glfwPollEvents();
 		glClearColor(.1f, .1f, .1f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//model = glm::rotate(model, (float)glfwGetTime() / 1000.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		int modelLoc = glGetUniformLocation(ourShader.program, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(ourShader.program, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(ourShader.program, "projection");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		
+		
 		// use shader program
-		our_shader.Use();
+		ourShader.Use();
+
+
+		
 
 		// draw
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 15);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
